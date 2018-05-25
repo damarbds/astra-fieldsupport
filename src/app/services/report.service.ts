146754b573ad294863b1c684621b01bf297359f2
Steven Yang <steven.yang@moonlay.jakarta>
2018-05-25 17:10:19 +0700

    Add filter data based on period on ticket status and user feedback report

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+import * as moment from "moment";
 import { environment } from "../../environments/environment";
 import {
   TicketStatusReport,
@@ -16,6 +17,10 @@ import {
 export class ReportService {
   constructor(private http: HttpClient) {}
 
+  convertDate(date) {
+    return moment.utc(date).format("YYYY-MM-DDTHH:mm:ss");
+  }
+
   getTicketStatuses(
     pageQuery: PageQuery,
     startDate: Date,
@@ -25,6 +30,12 @@ export class ReportService {
 
     queryString.push(`$top=${pageQuery.size}`);
     queryString.push(`$skip=${(pageQuery.page - 1) * pageQuery.size}`);
+
+    const formattedStartDate = this.convertDate(startDate);
+    const formattedEndDate = this.convertDate(endDate);
+    queryString.push(
+      `$filter=TicketCreatedDate ge datetime'${formattedStartDate}' and TicketCreatedDate le datetime'${formattedEndDate}'`
+    );
 
     return this.http.post<ApiResponseQuery<TicketStatusReport>>(
       `${environment.apiUrl}/api/report/ticket-status?${queryString.join("&")}`,
@@ -41,6 +52,12 @@ export class ReportService {
 
     queryString.push(`$top=${pageQuery.size}`);
     queryString.push(`$skip=${(pageQuery.page - 1) * pageQuery.size}`);
+
+    const formattedStartDate = this.convertDate(startDate);
+    const formattedEndDate = this.convertDate(endDate);
+    queryString.push(
+      `$filter=TicketCreatedDate ge datetime'${formattedStartDate}' and TicketCreatedDate le datetime'${formattedEndDate}'`
+    );
 
     return this.http.post<ApiResponseQuery<UserFeedbackReport>>(
       `${environment.apiUrl}/api/report/feedback-user?${queryString.join("&")}`,
